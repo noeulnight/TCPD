014d46864a62eb6f2ab1e9f137b51b9c9494e054
noeulnight <noeulnight.mail@gmail.com>
2021-01-22 16:54:30 +0900

    tlqkf

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const TClientsecret = 'm2ide0j5hoo15k1d61kamret6rw05f'
 const knex = require('knex')
 const express = require('express')
 const multer = require('multer')
+const fileupload = require('express-fileupload')
 const { createServer } = require('http')
 const path = require('path').resolve()
 const fetch = require('node-fetch')
@@ -23,6 +24,7 @@ const { renderFile: render } = require('ejs')
 const srv = createServer(app)
 const wss = socket(srv)
 
+app.use(fileupload())
 app.use('/uploads', express.static(path + '/uploads'))
 app.use('/public', express.static(path + '/public'))
 app.use(express.urlencoded({extended:false}))
@@ -60,7 +62,7 @@ app.get("/auth/twitch", passport.authenticate("twitch", { failureRedirect: "/" }
 app.post('/upload/img', auth, (req, res) => {
   const { id } = req.query
   if (!id) return res.sendStatus(400)
-  const upload = multer({ storage: storage, fileFilter: imageFilter }).single('profile')
+  let upload = multer({ storage: storage, fileFilter: imageFilter }).single('profile')
   upload(req, res, async (err) => {
     if (req.fileValidationError) return res.send('<script>alert("지원되지 않는 포맷입니다 (gif,png,jpg)."); location.href="/dashboard"</script>')
     else if (!req.file) return res.send('<script>alert("파일을 선택해주세요."); location.href="/dashboard"</script>').status(400)
@@ -80,19 +82,88 @@ app.get('/img/:id', async (req, res) => {
   res.redirect(exist.url)
 })
 
+const json = {
+  data: [
+    {
+      broadcaster_name: '메탈킴',
+      broadcaster_id: '46005422',
+      id: '7761ed12-1b51-4dfd-89cf-6b34cd035e4d',
+      image: null,
+      background_color: '#BEFF00',
+      is_enabled: true,
+      cost: 1000,
+      title: '자세 확인!',
+      prompt: '자세를 똑바로 합니다',
+      is_user_input_required: false,
+      max_per_stream_setting: [Object],
+      max_per_user_per_stream_setting: [Object],
+      global_cooldown_setting: [Object],
+      is_paused: false,
+      is_in_stock: true,
+      default_image: [Object],
+      should_redemptions_skip_request_queue: false,
+      redemptions_redeemed_current_stream: null,
+      cooldown_expires_at: null
+    },
+    {
+      broadcaster_name: '메탈킴',
+      broadcaster_id: '46005422',
+      id: '92effb15-e28e-4eb1-b998-516fabc26fba',
+      image: null,
+      background_color: '#00E5CB',
+      is_enabled: true,
+      cost: 3000,
+      title: '수분 섭취!',
+      prompt: '물 한 모금 마시기',
+      is_user_input_required: false,
+      max_per_stream_setting: [Object],
+      max_per_user_per_stream_setting: [Object],
+      global_cooldown_setting: [Object],
+      is_paused: false,
+      is_in_stock: true,
+      default_image: [Object],
+      should_redemptions_skip_request_queue: false,
+      redemptions_redeemed_current_stream: null,
+      cooldown_expires_at: null
+    },
+    {
+      broadcaster_name: '메탈킴',
+      broadcaster_id: '46005422',
+      id: 'a1dba36f-e986-4c01-b235-6f3dd2d6a4e2',
+      image: null,
+      background_color: '#C6B3FF',
+      is_enabled: true,
+      cost: 4000,
+      title: '스트레에에에에에에에칭',
+      prompt: '스트레칭으로 관절을 풀어줍니다 - 고마워요!',
+      is_user_input_required: false,
+      max_per_stream_setting: [Object],
+      max_per_user_per_stream_setting: [Object],
+      global_cooldown_setting: [Object],
+      is_paused: false,
+      is_in_stock: true,
+      default_image: [Object],
+      should_redemptions_skip_request_queue: false,
+      redemptions_redeemed_current_stream: null,
+      cooldown_expires_at: null
+    }
+  ]
+}
+
 app.get('/dashboard', auth, async (req, res) => {
   const [oauth] = await db.where({id:req.user.id}).from('oauth').select('*')
   if (!oauth) return res.redirect('/redirect/twitch')
-  fetch('https://api.twitch.tv/helix/channel_points/custom_rewards?broadcaster_id=' + req.user.id, {method:'GET', headers: { 'Client-Id': TClientid, 'authorization': 'Bearer ' + oauth.oauth }}).then(res => res.json()).then( async json => {
-    if (json.error) return res.send('<script>alert("채널포인트를 사용할수 없는 스트리머이거나, 트위치 연결중 오류가 발생했습니다. 나중에 다시 시도해주세요."); location.href="/"</script>')
-    json.data.forEach( async (v,i) => {
-      const [exist] = await db.where({ id: v.id, name: v.title, bid: v.broadcaster_id }).from('point2name').select('*')
-      exist ? await db.update({ name: v.title }).from('point2name').select('*').where({ id: v.id, bid: v.broadcaster_id}) : await db.insert({ id: v.id, name: v.title, bid: v.broadcaster_id }).from('point2name').select('*')
-    })
-    const str = await render(path + '/page/dashboard.ejs', { reward:json, user:req.user, setting:{url:'https://ablaze.noeul.codes/'} })
-    res.send(str)
+  if (json.error) return res.send('<script>alert("채널포인트를 사용할수 없는 스트리머이거나, 트위치 연결중 오류가 발생했습니다. 나중에 다시 시도해주세요."); location.href="/"</script>')
+  json.data.forEach( async (v,i) => {
+    const [exist] = await db.where({ id: v.id, name: v.title, bid: v.broadcaster_id }).from('point2name').select('*')
+    exist ? await db.update({ name: v.title }).from('point2name').select('*').where({ id: v.id, bid: v.broadcaster_id}) : await db.insert({ id: v.id, name: v.title, bid: v.broadcaster_id }).from('point2name').select('*')
   })
+  const str = await render(path + '/page/dashboard.ejs', { reward:json, user:req.user, setting:{url:'https://ablaze.noeul.codes/'} })
+  res.send(str)
+  // fetch('https://api.twitch.tv/helix/channel_points/custom_rewards?broadcaster_id=' + req.user.id, {method:'GET', headers: { 'Client-Id': TClientid, 'authorization': 'Bearer ' + oauth.oauth }}).then(res => res.json()).then( async json => {
+  // })
 })
+
 app.get('/widget/:code', async (req, res) => {
   const { code } = req.params
   if (!code) return res.redirect('/')
