8fb55b04b6c9f11dbaf851f036cf1b4318f71dd7
noeulnight <noeulnight.mail@gmail.com>
2021-01-28 18:02:09 +0900

    sql

--- database.sql
+++ database.sql
@@ -9,12 +9,9 @@ create table users (
   login varchar(30) not null,
   name varchar(30),
   email varchar(30),
+  accesstoken varchar(100) not null,
+  refreshtoken varchar(100) not null,
   createdAt timestamp default current_timestamp not null
-);
-
-create table oauth (
-  id varchar(30) not null primary key,
-  oauth varchar(30)
 );
 
 create table point2name (
